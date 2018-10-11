064e802e887ce76d1f8ddd21559bcde111363780
Константин Елагин <konstantinelagin@MacBook-Air-Konstantin.local>
2018-10-11 10:29:03 +0300

    styling in progress

--- bundle/js/project.js
+++ bundle/js/project.js
@@ -1,2 +1,168 @@
-!function(e){function t(o){if(n[o])return n[o].exports;var r=n[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,t),r.l=!0,r.exports}var n={};t.m=e,t.c=n,t.d=function(e,n,o){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:o})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},t.p="",t(t.s=0)}([function(e,t,n){"use strict";function o(e){if(e&&e.__esModule)return e;var t={};if(null!=e)for(var n in e)Object.prototype.hasOwnProperty.call(e,n)&&(t[n]=e[n]);return t.default=e,t}n(1),o((o(n(2)),n(3)))},function(e,t){},function(e,t,n){"use strict";var o=document.querySelector(".js-mobile-menu"),r=document.querySelector(".js-navigation");if(o&&r){var c=!0,u=function(e){e=e||window.event,27==e.keyCode&&l()},i=function(e){e=e||window.event;var t=e.target.getAttribute("href");(e.target.classList.contains("menu__link")||/^#/.test(t))&&l()},s=function(){o.querySelector(".menu-btn").classList.add("open"),r.classList.add("open"),document.body.classList.add("mobile-menu-opened"),document.addEventListener("keydown",u),document.addEventListener("click",i),c=!1},l=function(){o.querySelector(".menu-btn").classList.remove("open"),r.classList.remove("open"),document.body.classList.remove("mobile-menu-opened"),document.removeEventListener("keydown",u),document.removeEventListener("click",i),c=!0};o.onclick=function(){c?s():l()}}},function(e,t,n){"use strict";document.querySelectorAll(".js-smooth-link").forEach(function(e){e.addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href"),n=document.querySelector(t)||document.querySelector("a[name="+t.substring(1,t.length)+"]");window.scroll({top:n.offsetTop,left:0,behavior:"smooth"})})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
+
+var _mobile_menu = __webpack_require__(2);
+
+var mobile_menu = _interopRequireWildcard(_mobile_menu);
+
+var _smoothScroll = __webpack_require__(3);
+
+var smoothScroll = _interopRequireWildcard(_smoothScroll);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var mobileBtn = document.querySelector('.js-mobile-menu');
+var mobileMenu = document.querySelector('.js-navigation');
+
+if (!!mobileBtn && !!mobileMenu) {
+    var menuActive = true;
+
+    var onKeyDown = function onKeyDown(evt) {
+        evt = evt || window.event;
+        if (evt.keyCode == 27) {
+            closeMenu();
+        }
+    };
+
+    var onBodyClick = function onBodyClick(evt) {
+        evt = evt || window.event;
+        var targetHref = evt.target.getAttribute('href');
+        if (evt.target.classList.contains('menu__link') || /^#/.test(targetHref)) {
+            closeMenu();
+        }
+    };
+
+    var showMenu = function showMenu() {
+        mobileBtn.querySelector('.menu-btn').classList.add('open');
+        mobileMenu.classList.add('open');
+        document.body.classList.add('mobile-menu-opened');
+        document.addEventListener('keydown', onKeyDown);
+        document.addEventListener('click', onBodyClick);
+        menuActive = false;
+    };
+    var closeMenu = function closeMenu() {
+        mobileBtn.querySelector('.menu-btn').classList.remove('open');
+        mobileMenu.classList.remove('open');
+        document.body.classList.remove('mobile-menu-opened');
+        document.removeEventListener('keydown', onKeyDown);
+        document.removeEventListener('click', onBodyClick);
+        menuActive = true;
+    };
+
+    mobileBtn.onclick = function () {
+        console.log('123');
+        if (menuActive) {
+            showMenu();
+        } else {
+            closeMenu();
+        }
+    };
+}
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+document.querySelectorAll('.js-smooth-link').forEach(function (a) {
+    a.addEventListener('click', function (e) {
+        e.preventDefault();
+        var href = this.getAttribute("href");
+        var elem = document.querySelector(href) || document.querySelector("a[name=" + href.substring(1, href.length) + "]");
+        window.scroll({
+            top: elem.offsetTop,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
+
+/***/ })
+/******/ ]);
 //# sourceMappingURL=project.js.map
